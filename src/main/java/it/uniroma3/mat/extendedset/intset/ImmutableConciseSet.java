c0e37dd54f58f2cef6fc7bef2999d3e55d321fb7
Fangjin Yang <fangjin@metamarketsgroup.com>
2012-01-30 11:31:16 -0800

    temp null fix; this will get changed soon

--- src/main/java/it/uniroma3/mat/extendedset/intset/ImmutableConciseSet.java
+++ src/main/java/it/uniroma3/mat/extendedset/intset/ImmutableConciseSet.java
@@ -133,8 +133,9 @@ public class ImmutableConciseSet extends AbstractIntSet implements java.io.Seria
 
   public ImmutableConciseSet(ConciseSet conciseSet)
   {
-    this.words = conciseSet.isEmpty() ? null : IntBuffer.wrap(conciseSet.getWords());
-    this.lastWordIndex = conciseSet.isEmpty() ? -1 : this.words.capacity() - 1;
+    boolean nullSet = conciseSet == null || conciseSet.isEmpty();
+    this.words = nullSet ? null : IntBuffer.wrap(conciseSet.getWords());
+    this.lastWordIndex = nullSet ? -1 : this.words.capacity() - 1;
     this.size = -1;
     this.simulateWAH = false;
   }
